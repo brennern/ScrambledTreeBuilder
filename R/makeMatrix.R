15f60bfa964c9f4f3aec3c290b3eb7f69c9e8590
Noa <brennern@hawaii.edu>
2023-07-28 16:15:51 +0900

    commit

--- R/makeMatrix.R
+++ R/makeMatrix.R
@@ -1,3 +1,15 @@
+#' Make a matrix from a data frame containing species data.
+#'
+#' @param df Data frame containing species information.
+#' @param column The variable from the data frame you want to extract species data from.
+#' @param defaultDiagonal defaultDiagonal
+#' @param defaultValue defaultValue
+#'
+#' @return A matrix containing species names and data for the selected variable.
+#' @export
+#'
+#' @examples
+#' m <- makeMatrix(df, "percent_identity_global", 100, 50)
 makeMatrix <- function(df, column, defaultDiagonal = 100, defaultValue = NA) {
   species <- unique(df$species2)
   m <- matrix(defaultValue, nrow=length(species), ncol=length(species))
