b45054c80ff7a6fb2ad270441b55e5418ca82273
Noa <brennern@hawaii.edu>
2023-07-31 14:14:51 +0900

    new function

---
+++ R/formatStats.R
@@ -0,0 +1,19 @@
+#' Format species data and calculate percent identity and strand randomisation variables as part of the dataframe.
+#'
+#' @param df
+#'
+#' @return Formatted dataframe with strand randomisation and percent identities between species calculated.
+#' @export
+#'
+#' @examples
+#' df <- formatStats(df)
+formatStats <- function(df) {
+  df <- do.call(rbind, lapply(yamlFiles, getStats)) |> as.data.frame()
+  df <- df[,colSums(df, na.rm = TRUE) !=0]
+  df$species1 <- strsplit(rownames(df), "___") |> lapply(\(.) .[1]) |> unlist()
+  df$species2 <- strsplit(rownames(df), "___") |> lapply(\(.) .[2]) |> unlist()
+  df <- df[df$species1 != df$species2,]
+  df$index_avg_strandRand <- (df$index_strandRand_target_bestpair + df$index_strandRand_query_bestpair) / 2
+  df$percent_identity_global <- df$aligned_matches_bestpair_Total / df$aligned_length_bestpair_Total * 100
+  df
+}
