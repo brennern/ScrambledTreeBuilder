e866ccdc164e00df1a825eadd9a09dc3e3598073
Noa <brennern@hawaii.edu>
2023-08-03 16:03:27 +0900

    commit

--- R/extractPercentID.R
+++ R/extractPercentID.R
@@ -16,17 +16,17 @@
 #' TD$percentID <- NA
 #' TD[names(pIDs), "percentID"] <- unname(pIDs)
 extractPercentID <- function(node, tibble, matrix, fun = mean) {
-  children <- child(tibble, node)
+  children <- tidytree::child(tibble, node)
   stopifnot(nrow(children) == 2)
   left_side_node  <- children[1, "node", drop = TRUE]
   right_side_node <- children[2, "node", drop = TRUE]
   tipLabels <- function(node, tibble) {
     tipLabel <- tibble[node,"label", drop = TRUE]
     if(!is.na(tipLabel)) return(tipLabel)
-    offspring(tibble, node)$label |> Filter(complete.cases, x=_)
+    tidytree::offspring(tibble, node)$label |> Filter(complete.cases, x=_)
   }
-  left_side_species  <- tipLabels(left_side_node,  tibble)
-  right_side_species <- tipLabels(right_side_node, tibble)
+  left_side_species  <- tidytree::tipLabels(left_side_node,  tibble)
+  right_side_species <- tidytree::tipLabels(right_side_node, tibble)
   comparison <- matrix[left_side_species, right_side_species, drop=F]
   percentID <- fun(comparison)
   percentID
