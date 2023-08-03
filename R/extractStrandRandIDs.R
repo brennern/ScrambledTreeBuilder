d73161fc1fc80062589c57a61640f4d98e18647f
Noa <brennern@hawaii.edu>
2023-08-03 15:55:21 +0900

    commit

--- R/extractStrandRandIDs.R
+++ R/extractStrandRandIDs.R
@@ -17,17 +17,17 @@
 #' TD$strandRand <- NA
 #' TD[names(sRIs), "strandRand"] <- unname(sRIs)
 extractStrandRandIDs <- function(node, tibble, matrix, fun = mean) {
-  children <- child(tibble, node)
+  children <- tidytree::child(tibble, node)
   stopifnot(nrow(children) == 2)
   left_side_node  <- children[1, "node", drop = TRUE]
   right_side_node <- children[2, "node", drop = TRUE]
   tipLabels <- function(node, tibble) {
     tipLabel <- tibble[node,"label", drop = TRUE]
     if(!is.na(tipLabel)) return(tipLabel)
-    offspring(tibble, node)$label |> Filter(complete.cases, x=_)
+    tidytree::offspring(tibble, node)$label |> Filter(complete.cases, x=_)
   }
-  left_side_species  <- tipLabels(left_side_node,  tibble)
-  right_side_species <- tipLabels(right_side_node, tibble)
+  left_side_species  <- tidytree::tipLabels(left_side_node,  tibble)
+  right_side_species <- tidytree::tipLabels(right_side_node, tibble)
   comparison <- matrix[left_side_species, right_side_species, drop=F]
   strandRand <- fun(comparison)
   strandRand
