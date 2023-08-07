e650396d1f6197cc8a5347c0f10da4a7be256bec
Noa <brennern@hawaii.edu>
2023-08-07 11:50:21 +0900

    New extractColumnLabels function to work on: general function to replace extractStrandRand and extractPercentID

---
+++ R/extractColumnLabels.R
@@ -0,0 +1,45 @@
+#' Extract labels from a dataframe column
+#'
+#' @param node
+#' @param dataframe Dataframe containing species data.
+#' @param column The variable from the data frame you want to extract species data from.
+#' @param fun Calculates mean, median, etc. between nodes on the tree (default fun is mean)
+#'
+#' @return Scores to store in a variable (percent identity, strand randomisation index, etc.)
+#' @export
+#'
+#' @examples
+#' percentID <- extractColumnLabel(exDataFrame, "percent_identity_global")
+extractColumnLabel <- function(node, dataframe, column, fun = mean) {
+  makeMatrix <- function(dataframe, column, defaultDiagonal = 100, defaultValue = NA) {
+    species <- unique(dataframe$species2)
+    matrix <- matrix(defaultValue, nrow=length(species), ncol=length(species))
+    colnames(matrix) <- rownames(matrix) <- species
+    for (i in 1:length(species)) {
+      matrix[i,i] <- defaultDiagonal
+    }
+    for (i in 1:nrow(df)) {
+      s1 <- df[i, "species1"]
+      s2 <- df[i, "species2"]
+      if(s1 %in% species)
+        matrix[s1, s2] <- dataframe[i, column]
+    }
+    matrix
+  }
+  HClust <- hclust(dist(matrix), method = "complete")
+  Dend <- as.dendrogram(HClust)
+  tibble <- as_tibble(as.phylo(Dend))
+  children <- child(tibble, node)
+  stopifnot(nrow(children) == 2)
+  left_side_node  <- children[1, "node", drop = TRUE]
+  right_side_node <- children[2, "node", drop = TRUE]
+  tipLabels <- function(node, tibble) {
+    tipLabel <- tibble[node,"label", drop = TRUE]
+    if(!is.na(tipLabel)) return(tipLabel)
+    offspring(tibble, node)$label |> Filter(complete.cases, x=_)
+  }
+  left_side_species  <- tipLabels(left_side_node,  tibble)
+  right_side_species <- tipLabels(right_side_node, tibble)
+  comparison <- matrix[left_side_species, right_side_species, drop=F]
+  fun(comparison)
+}
