e9b207a84eded757ed4b08bc1c430070bbcdbd0a
Noa <brennern@hawaii.edu>
2023-08-14 17:02:20 +0900

    update visualizeTree.R

--- R/visualizeTree.R
+++ R/visualizeTree.R
@@ -10,6 +10,6 @@
 visualizeTree <- function(tibble, value) {
   ggtree::ggtree(tidytree::as.treedata(tibble)) +
     ggtree::geom_tiplab(as_ylab=TRUE) +
-    ggtree::geom_label(ggtree::aes(label=round(value, digit = 2), color = value), label.size = 0.25, size = 2, na.rm = TRUE, label.padding = ggtree::unit(0.15, "lines")) +
+    ggtree::geom_label(ggtree::aes(label=round(value, digit = 2), color = value), label.size = 0.25, size = 6, na.rm = TRUE, label.padding = ggtree::unit(0.15, "lines")) +
     viridis::scale_color_viridis()
 }
