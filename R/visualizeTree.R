1b11b7171cf023772b1069a265813f54ee1977a5
Noa <brennern@hawaii.edu>
2023-09-05 11:18:22 +0900

    visualizeTree.R update for more argument options

--- R/visualizeTree.R
+++ R/visualizeTree.R
@@ -7,8 +7,10 @@
 #'
 #' @examples
 #' Tree <- visualizeTree(your_tibble = tibbleWithValue)
-visualizeTree <- function(your_tibble, value) {
+visualizeTree <- function(your_tibble, value, valueround = 2, outerlabelsize = 0.25, innerlabelsize = 3, ynudge = 0, xnudge = 0) {
   ggtree::ggtree(tidytree::as.treedata(your_tibble)) +
-    ggtree::geom_tiplab(as_ylab=TRUE) +
-    ggtree::geom_label(ggtree::aes(label=round(value, digits = 2), color = value), label.size = 0.25, size = 3.5, na.rm = TRUE, label.padding = ggtree::unit(0.15, "lines"))
+  ggtree::geom_tiplab(as_ylab=TRUE) +
+  ggtree::geom_label(ggtree::aes(label=round(value, digits = valueround), color = value), label.size = outerlabelsize,
+                     size = innerlabelsize, na.rm = TRUE, label.padding = ggtree::unit(0.15, "lines"),
+                     nudge_y = ynudge, nudge_x = xnudge)
 }
