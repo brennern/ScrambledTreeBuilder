07267d81e2ec7768dfd0ff3278aa590c4932a226
Charles Plessy <charles.plessy@oist.jp>
2024-11-13 18:13:34 +0900

    Fix subTree() by using tiditree::tree_subset

--- R/subTree.R
+++ R/subTree.R
@@ -5,23 +5,17 @@
 #' @param name description
 #' @param node_id The ID number of the node to base the subtree.
 #'
-#' @importFrom tidytree offspring as_tibble as.treedata
-#' @importFrom dplyr bind_rows
+#' @importFrom tidytree as_tibble as.treedata tree_subset
 #'
 #' @return A subtree represented by a tibble created by the tidytree package.
 #'
 #' @examples
-#' subTree(tibbleWithValue, 9)
+#' (subtree <- subTree(tibbleWithValue, 9))
+#' visualizeTree(subtree, subtree$node.orig)
 #'
 #' @export
 
-subTree <- function(tree_tibble, node_id) {
+subTree <- function(tree_tibble, node) {
   tree_tibble$node.orig <- tree_tibble$node
-  descendants <- tidytree::offspring(tree_tibble, .node = node_id)
-  descendants_with_node <- dplyr::bind_rows(
-    filter(tree_tibble, node == node_id),
-    descendants) |> as.treedata() |> as_tibble() |> suppressMessages()
-  descendants_with_node$label <- descendants_with_node$label.y
-  descendants_with_node$label.y <-NULL
-  descendants_with_node
+  tree_tibble |> as.treedata() |> tree_subset(node, levels_back = 0) |> as_tibble()
 }
