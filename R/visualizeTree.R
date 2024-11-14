71ac183b0760248e26be77e0f85721c532abdaab
Charles Plessy <charles.plessy@oist.jp>
2024-11-14 11:52:05 +0900

    Allow passing values as a column name.

--- R/visualizeTree.R
+++ R/visualizeTree.R
@@ -1,7 +1,8 @@
 #' Create a phylogenetic tree using tibble data.
 #'
 #' @param your_tibble Tibble data containing species genome information.
-#' @param value Tibble value to label on internal nodes of the tree.
+#' @param value Tibble value to label on internal nodes of the tree, or name
+#'        of a column in the tree object.
 #' @param valueround Number of integers to round value.
 #' @param outerlabelsize Size of label border.
 #' @param innerlabelsize Overall size of label.
@@ -14,7 +15,11 @@
 #' @examples
 #' visualizeTree(tibbleWithValue)
 #' visualizeTree(tibbleWithValue, value = tibbleWithValue$Strand_Randomisation_Index)
-visualizeTree <- function(your_tibble, value=your_tibble$node, valueround = 2, outerlabelsize = 0.25, innerlabelsize = 3, ynudge = 0, xnudge = 0) {
+#' visualizeTree(tibbleWithValue, "Strand_Randomisation_Index")
+
+visualizeTree <- function(your_tibble, value="node", valueround = 2, outerlabelsize = 0.25, innerlabelsize = 3, ynudge = 0, xnudge = 0) {
+  if (length(value == 1) && is.character(value))
+    value <- your_tibble[ , value, drop = TRUE]
   ggtree::ggtree(tidytree::as.treedata(your_tibble)) +
   ggtree::geom_tiplab(as_ylab=TRUE) +
   ggtree::geom_label(ggtree::aes(label=round(value, digits = valueround), color = value), label.size = outerlabelsize,
