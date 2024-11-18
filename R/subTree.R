8fc28e3fce32646b67c6919eb1d90635b650a724
Charles Plessy <charles.plessy@oist.jp>
2024-11-18 14:57:14 +0900

    Fix subTree() documentation

--- R/subTree.R
+++ R/subTree.R
@@ -2,7 +2,7 @@
 #'
 #' Takes a tibble representing a tree, and extracts a subtree at a given node.
 #'
-#' @param name A tree in the `tbl_tree` class of the _tidytree_ package.
+#' @param tree_tibble A tree in the `tbl_tree` class of the _tidytree_ package.
 #' @param node The ID number of the node to base the subtree.
 #'
 #' @importFrom tidytree as_tibble as.treedata tree_subset
@@ -20,5 +20,5 @@
 
 subTree <- function(tree_tibble, node) {
   tree_tibble$node.orig <- tree_tibble$node
-  tree_tibble |> as.treedata() |> tree_subset(node, levels_back = 0) |> as_tibble()
+  tree_tibble |> as.treedata() |> tree_subset(node, levels_back = 0) |> as_tibble() |> suppressMessages()
 }
