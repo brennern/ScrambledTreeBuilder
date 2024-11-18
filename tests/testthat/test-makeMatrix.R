dfd56aaf28cc00cfdc467e4fed2d4a1754a94d12
Charles Plessy <charles.plessy@oist.jp>
2024-11-18 14:56:53 +0900

    Fix stuff in the tests folder

--- tests/testthat/test-makeMatrix.R
+++ tests/testthat/test-makeMatrix.R
@@ -1,3 +1,5 @@
 test_that("output is a matrix", {
-  expect_snapshot_output(is.matrix(matrix))
+  expect_snapshot_output(is.matrix(
+    makeMatrix(exDataFrame, "percent_identity_global", 100, 50)
+  ))
 })
--- tests/testthat/test-makeValueTibble.R
+++
@@ -1,3 +0,0 @@
-test_that("column is added to original tibble", {
-  expect_identical(input, output) = FALSE
-})
