92c4d70b44f61c11f6fd740bf2054057f0804c99
Charles Plessy <charles.plessy@oist.jp>
2025-06-17 09:55:18 +0900

    Do not plot a legend if displaying node IDs.
    Plus additional minor changes.

--- R/visualizeTree.R
+++ R/visualizeTree.R
@@ -1,15 +1,17 @@
-#' Create a phylogenetic tree using tibble data.
+#' Plot a phylogenetic tree and its node data
 #'
-#' @param your_tibble Tibble data containing species genome information.
+#' @param your_tibble A `tbl_tree` object produced by the [`makeTidyTree`] or
+#'        [`makeValueTibble`] functions.
 #' @param value Tibble value to label on internal nodes of the tree, or name
 #'        of a column in the tree object.
 #' @param valueround Number of integers to round value.
 #' @param outerlabelsize Size of label border.
 #' @param innerlabelsize Overall size of label.
-#' @param ynudge Adjust vertical position of labels.
-#' @param xnudge Adjust horizontal position of labels.
+#' @param xnudge,ynudge Adjust horizontal or vertical position of labels
+#'        (useful when plotting multiple labels).
 #'
-#' @return A phylogenetic tree showcasing phylogeny of species and additional calculated values.
+#' @return A phylogenetic tree showcasing phylogeny of species, additional node
+#' values, or just node IDs.  No legend is show when displaying node IDs.
 #' @export
 #'
 #' @examples
@@ -18,11 +20,18 @@
 #' visualizeTree(tibbleWithValue, "Strand_Randomisation_Index")
 
 visualizeTree <- function(your_tibble, value="node", valueround = 2, outerlabelsize = 0.25, innerlabelsize = 3, ynudge = 0, xnudge = 0) {
-  if (length(value == 1) && is.character(value))
+  noLegend <- FALSE
+  if (length(value) == 1 && value == "node") noLegend <- TRUE
+  if (length(value) == 1 && is.character(value))
     value <- your_tibble[ , value, drop = TRUE]
-  ggtree::ggtree(tidytree::as.treedata(your_tibble)) +
-  ggtree::geom_tiplab(as_ylab=TRUE) +
-  ggtree::geom_label(ggtree::aes(label=round(value, digits = valueround), color = value), label.size = outerlabelsize,
-                     size = innerlabelsize, na.rm = TRUE, label.padding = ggtree::unit(0.15, "lines"),
-                     nudge_y = ynudge, nudge_x = xnudge)
+  gg <- ggtree::ggtree(tidytree::as.treedata(your_tibble)) +
+        ggtree::geom_tiplab(as_ylab=TRUE) +
+        ggtree::geom_label(ggtree::aes(label=round(value, digits = valueround), color = value), label.size = outerlabelsize,
+                           size = innerlabelsize, na.rm = TRUE, label.padding = ggtree::unit(0.15, "lines"),
+                           nudge_y = ynudge, nudge_x = xnudge)
+  if (noLegend) {
+    gg + ggplot2::theme(legend.position = "none")
+  } else {
+    gg
+  }
 }
