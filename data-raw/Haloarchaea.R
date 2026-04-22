389a13551fd926d3103000bc03c8fe257ce4fa7f
Charles Plessy <charles.plessy@oist.jp>
2026-04-23 08:44:26 +0900

    Give same name and description to example focal clades.
    Having them different triggers bugs that are not sovled yet.

--- data-raw/Haloarchaea.R
+++ data-raw/Haloarchaea.R
@@ -13,11 +13,12 @@ Halo_Tree <- makeTidyTree((Halo_PercentDiff/2 + t(Halo_PercentDiff)/2)) |>
   makeValueTibble(Halo_ScramblIdx,  colname = "Scrambling_index")
 usethis::use_data(Halo_Tree, overwrite = TRUE)
 
+Halo_bacterium   <- focalClade(Halo_Tree, "Halobacterium_noricense", "Halobacterium_salinarum", "blue",   "Halobacterium")
+Halo_ferax       <- focalClade(Halo_Tree, "Haloferax_mediterranei",  "Haloferax_volcanii",      "green3", "Haloferax")
+Halo_FocalClades <- FocalCladeList(Halobacterium = Halo_bacterium, Haloferax = Halo_ferax)
+
 Halo_DF <- recordAncestor(Halo_DF, Halo_Tree)
 Halo_DF <- recordClades  (Halo_DF, Halo_FocalClades)
 usethis::use_data(Halo_DF, overwrite = TRUE)
 
-Halo_bacterium   <- focalClade(Halo_Tree, "Halobacterium_noricense", "Halobacterium_salinarum", "blue",   "Halobacterium genus")
-Halo_ferax       <- focalClade(Halo_Tree, "Haloferax_mediterranei",  "Haloferax_volcanii",      "green3", "Haloferax genus")
-Halo_FocalClades <- FocalCladeList(Halobacterium = Halo_bacterium, Haloferax = Halo_ferax)
 usethis::use_data(Halo_FocalClades, overwrite = TRUE)
